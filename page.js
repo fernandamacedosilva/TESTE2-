3807882694841a43287dc41ec977025649241892
FERNANDA MACEDO <macedo.nanda98@gmail.com>
2025-06-27 21:11:25 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Inicio (){
+    return (
+        <div>
+            <h1>MEU SITE</h1>
+            <Link href="/home">ir para home</Link>
+            <Link href="/galeria">ir para galeria</Link>
+            <Link href="/contato">ir para contato</Link>
+        
+        </div>
+    ); 
+}
